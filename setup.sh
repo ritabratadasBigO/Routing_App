452a3ae7d4cc887fb267383d5a844def5b7f7f47
niteowl1986 <ritabratadas12@gmail.com>
2025-05-23 01:39:36 +0530

    Fixed requirements.txt and escape $PORT properly in setup.sh for Heroku

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[server]\n\
-headless = true\n\
-port = \$PORT\n\
-enableCORS = false\n\
-\n\
+[server]
+headless = true
+port = \$PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
